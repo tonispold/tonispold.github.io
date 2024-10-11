a30f828e796e2f4496153e4a8fea7fb92f5bb447
tonispold <tonis123@tlu.ee>
2024-10-11 12:07:03 +0300

    second commit

--- main.js
+++ main.js
@@ -57,51 +57,19 @@ function sectionChange(elem) {
 }
 
 
-function readMoreFirst() {
-    var dots = document.getElementById("dots-first");
-    var moreText = document.getElementById("more-first");
-    var btnText = document.getElementById("firstBtn");
+function readMore(dotsId, moreId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreId);
+  var btnText = document.getElementById(btnId);
 
   if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
   } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
-
-function readMoreSecond() {
-    var dots = document.getElementById("dots-second");
-    var moreText = document.getElementById("more-second");
-    var btnText = document.getElementById("secondBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
-
-function readMoreThird() {
-    var dots = document.getElementById("dots-third");
-    var moreText = document.getElementById("more-third");
-    var btnText = document.getElementById("thirdBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
   }
 }
 
